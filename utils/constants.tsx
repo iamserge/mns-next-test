ce4f124857bac02549227cba23dcc2fae69d5f07
sradkevics <sradkevics@wundermancommerce.com>
2020-10-27 14:58:12 +0000

    Updating production API url

--- utils/constants.tsx
+++ utils/constants.tsx
@@ -1,4 +1,4 @@
 export const LOGGEDIN_USER_KEY = "LOGGEDIN_USER";
 export const ITEMS_PER_PAGE = 12;
 export const LOCAL_URL = 'http://localhost:3001/graphql';
-export const DOCKER_URL = 'http://mns_api_1:3001/graphql';
+export const DOCKER_URL = 'http://mns-next-test_api_1:3001/graphql';
